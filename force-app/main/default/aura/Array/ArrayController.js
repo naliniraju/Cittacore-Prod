bacd9f29a77a475335c3572856a5d1259a25709a
nagurukarimulla@gmail.com <nagurukarimulla@gmail.com>
2022-10-03 10:36:23 +0530

    karim field added

---
+++ force-app/main/default/aura/Array/ArrayController.js
@@ -0,0 +1,12 @@
+/** Client-side Controller **/
+({
+  getNumbers: function(component, event, helper) {
+    var numbers = [];
+    for (var i = 0; i < 20; i++) {
+      numbers.push({
+        value: i
+      });
+    }
+    component.set("v.numbers", numbers); 
+    }
+})
